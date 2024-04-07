d2fac6d1d40d5f57dabb4d64eb7ecd27869b2291
Konevozd <nemanja543210@gmail.com>
2024-04-07 20:03:03 +0200

    Added roles in redux

---
+++ src/actions/roles.js
@@ -0,0 +1,20 @@
+import * as notifications from "../utils/notification";
+import apiService from "../utils/apiService";
+import { movieRolesPath } from "../utils/endpoints";
+import { rolesActions } from "../reducers/roles";
+
+export const getRoles = (movieID) => {
+  return (dispatch) => {
+    dispatch(rolesActions.actionStart());
+    console.log(movieID);
+    return apiService
+      .get(movieRolesPath(movieID))
+      .then((response) => {
+        dispatch(rolesActions.fetchRoles(response.data));
+      })
+      .catch((error) => {
+        dispatch(rolesActions.actionError(error?.response?.data));
+        notifications.error();
+      });
+  };
+};
