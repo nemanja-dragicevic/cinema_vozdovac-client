59e4b63542cfc2b4aa84aba2c704d458a132989e
nemanja-dragicevic <nd201900065@student.fon.bg.ac.rs>
2024-04-17 19:51:49 +0200

    updated notifications

--- src/utils/notification.js
+++ src/utils/notification.js
@@ -1,10 +1,10 @@
 import { toast } from "react-toastify";
 
 export function error(message) {
-  toast.error(message);
+  toast.error(message, { closeOnClick: true, theme: "colored" });
   // .success .info
 }
 
 export function success(message) {
-  toast.success(message);
+  toast.success(message, { closeOnClick: true, theme: "colored" });
 }
