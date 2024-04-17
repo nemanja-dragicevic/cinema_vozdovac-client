ded54a588961f13a507b8d812b62a2d0a9148f66
nemanja-dragicevic <nd201900065@student.fon.bg.ac.rs>
2024-04-17 19:31:19 +0200

    done responsive navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { Link, NavLink } from "react-router-dom";
 
-const NavBar = ({ token }) => {
+const NavBar = () => {
+  const { member } = useSelector((state) => state.membersReducer);
+
+  useEffect(() => {
+    console.log("Member changed:", member);
+  }, [member]);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <Link className="navbar-brand" to="/">
@@ -11,32 +18,67 @@ const NavBar = ({ token }) => {
         className="navbar-toggler"
         type="button"
         data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
+        data-target="#navbarText"
+        aria-controls="navbarText"
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span className="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarText">
-        <ul className="navbar-nav ">
-          <li className="nav-item active">
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav ">
+          <li class="nav-item active">
             <NavLink className="nav-link" to="/movies">
               Naši Filmovi
             </NavLink>
           </li>
-
-          <li className="nav-item">
+          <li class="nav-item">
             <NavLink className="nav-link" to="/register">
               Profile
             </NavLink>
           </li>
         </ul>
-        <span className="navbar-text" style={{ marginLeft: 20 }}>
-          Gledajte najbolje filmove na svetu na jednom mestu
-        </span>
+        <span class="navbar-text">Dobrodošli</span>
       </div>
     </nav>
+    // <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    //   <Link className="navbar-brand" to="/">
+    //     Bioskop Vozdovac
+    //   </Link>
+    //   <button
+    //     className="navbar-toggler"
+    //     type="button"
+    //     data-toggle="collapse"
+    //     data-target="#navbarNav"
+    //     aria-controls="navbarNav"
+    //     aria-expanded="false"
+    //     aria-label="Toggle navigation"
+    //   >
+    //     <span className="navbar-toggler-icon"></span>
+    //   </button>
+    //   <div className="collapse navbar-collapse" id="navbarText">
+    //     <ul className="navbar-nav ">
+    //       <li className="nav-item active">
+    //         <NavLink className="nav-link" to="/movies">
+    //           Naši Filmovi
+    //         </NavLink>
+    //       </li>
+
+    //       {!localStorage.getItem("token") ? (
+    //         <li className="nav-item">
+    //           <NavLink className="nav-link" to="/register">
+    //             Profile
+    //           </NavLink>
+    //         </li>
+    //       ) : (
+    //         <li></li>
+    //       )}
+    //     </ul>
+    //     <span className="navbar-text" style={{ marginLeft: 20 }}>
+    //       Gledajte najbolje filmove na svetu na jednom mestu
+    //     </span>
+    //   </div>
+    // </nav>
   );
 };
 
