d4e4a8b249bfa022eaf898d7d18654afebd35adc
nemanja-dragicevic <nd201900065@student.fon.bg.ac.rs>
2024-05-02 22:41:18 +0200

    Added save, update and delete operations

--- src/actions/hall.js
+++ src/actions/hall.js
@@ -2,6 +2,7 @@ import { hallActions } from "../reducers/hall";
 import apiService from "../utils/apiService";
 import * as notifications from "../utils/notification";
 import { hallsPath } from "../utils/endpoints";
+import { redirect401Error } from "../utils/redirect401Error";
 
 export const getHalls = () => {
   return (dispatch) => {
@@ -13,7 +14,64 @@ export const getHalls = () => {
       })
       .catch((error) => {
         if (error.response.status === 401) {
-          localStorage.removeItem("token");
+          redirect401Error(error);
+        }
+        dispatch(hallActions.actionError(error?.response?.data));
+        notifications.error();
+      });
+  };
+};
+
+export const saveHall = (data) => {
+  return (dispatch) => {
+    dispatch(hallActions.actionStart());
+    return apiService
+      .post(hallsPath, data)
+      .then((response) => {
+        dispatch(hallActions.addHall(response.data));
+        notifications.success("Hall added successfully!");
+      })
+      .catch((error) => {
+        if (error.response.status === 401) {
+          redirect401Error(error);
+        }
+        dispatch(hallActions.actionError(error?.response?.data));
+        notifications.error();
+      });
+  };
+};
+
+export const updateHall = (data) => {
+  return (dispatch) => {
+    dispatch(hallActions.actionStart());
+    return apiService
+      .put(hallsPath, data)
+      .then((response) => {
+        dispatch(hallActions.updateHall(response.data));
+        notifications.success("Hall updated successfully!");
+      })
+      .catch((error) => {
+        if (error.response.status === 401) {
+          redirect401Error(error);
+        }
+        dispatch(hallActions.actionError(error?.response?.data));
+        notifications.error();
+      });
+  };
+};
+
+export const deleteHall = (hallID) => {
+  return (dispatch) => {
+    dispatch(hallActions.actionStart());
+    return apiService
+      .delete(`${hallsPath}/${hallID}`)
+      .then(() => {
+        dispatch(hallActions.deleteHall(hallID));
+        notifications.success("Hall deleted successfully!");
+      })
+      .catch((error) => {
+        if (error.response.status === 401) {
+          redirect401Error(error);
         }
         dispatch(hallActions.actionError(error?.response?.data));
         notifications.error();
