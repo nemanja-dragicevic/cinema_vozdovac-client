702ebccd7ec785e8b6ae8ed3911abdba87d4eb33
nemanja-dragicevic <nd201900065@student.fon.bg.ac.rs>
2024-07-01 19:07:23 +0200

    get movie projections for ticket selection

--- src/actions/projections.js
+++ src/actions/projections.js
@@ -133,3 +133,19 @@ export const getProjectionsForDateAndMovie = (date, id) => {
       });
   };
 };
+
+export const getProjectionsForMovie = (id) => {
+  return (dispatch) => {
+    dispatch(projectionsActions.actionStart());
+    return apiService
+      .get(projectionsPath + `/projections/${id}`)
+      .then((response) => {
+        dispatch(projectionsActions.fetchProjections(response.data));
+      })
+      .catch((error) => {
+        // if (error?.response?.status === 401) redirect401Error(error);
+        dispatch(projectionsActions.actionError(error?.response?.data));
+        notifications.error(error?.response?.data);
+      });
+  };
+};
