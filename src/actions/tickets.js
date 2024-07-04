0e5c163b2b020a255327ed392809ab69ab55e699
nemanja-dragicevic <nd201900065@student.fon.bg.ac.rs>
2024-07-04 16:56:44 +0200

    saving tickets selection

--- src/actions/tickets.js
+++ src/actions/tickets.js
@@ -3,8 +3,9 @@ import apiService from "../utils/apiService";
 import { ticketsPath } from "../utils/endpoints";
 import { ticketActions } from "../reducers/ticket";
 
-export const saveCheckoutTickets = (ticket) => {
+export const saveCheckoutTickets = (ticketItem, projectionId) => {
   return (dispatch) => {
-    dispatch(ticketActions.addTicket(ticket));
+    dispatch(ticketActions.deleteWIthProjectionId(projectionId));
+    dispatch(ticketActions.addTicketItem(ticketItem));
   };
 };
--- src/reducers/ticket.js
+++ src/reducers/ticket.js
@@ -18,9 +18,50 @@ const ticketSlice = createSlice({
       state.ticket = action.payload;
       state.error = undefined;
     },
-    addTicket(state, action) {
-      state.checkout = [action.payload].concat(state.tickets);
+    deleteWIthProjectionId(state, action) {
+      state.checkout = state.checkout.filter(
+        (item) => item.projectionId !== action.payload
+      );
       state.error = undefined;
+    },
+    addTicketItem(state, action) {
+      // state.checkout = state.checkout.push(action.payload);
+      // state.error = undefined;
+      const { checkout } = state;
+
+      // // Filter out any existing ticket items with the same projectionId
+      // const updatedCheckout = checkout.filter(
+      //   (item) =>
+      //     item.ticketItem[0].projectionId !==
+      //     action.payload.ticketItem[0].projectionId
+      // );
+
+      // // Push the new action.payload (new ticket item) to the filtered array
+      // updatedCheckout.push(action.payload);
+
+      // // Update the state with the new checkout array and clear any errors
+      // return {
+      //   ...state,
+      //   checkout: updatedCheckout,
+      //   error: undefined,
+      // };
+
+      // const updatedCheckout = checkout.filter(
+      //   (item) => item.projectionId !== action.payload.projectionId
+      // );
+      // // updatedCheckout.push(action.payload);
+      // updatedCheckout.concat(action.payload);
+
+      // return {
+      //   ...state,
+      //   checkout: updatedCheckout,
+      //   error: undefined,
+      // };
+      return {
+        ...state,
+        checkout: [...checkout, ...action.payload],
+        error: undefined,
+      };
     },
     deleteTicket(state, action) {
       state.ticket = state.ticket.filter(
