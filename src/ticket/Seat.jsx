caa386c04c8718a504266daf1a00061347e64631
nemanja-dragicevic <nd201900065@student.fon.bg.ac.rs>
2024-07-04 18:05:36 +0200

    updated Seat component

--- src/ticket/Seat.jsx
+++ src/ticket/Seat.jsx
@@ -1,12 +1,12 @@
 import ChairIcon from "@mui/icons-material/Chair";
 import ChairOutlinedIcon from "@mui/icons-material/ChairOutlined";
 
-const Seat = ({ row, seat, isSelected, toggleSeat }) => {
+const Seat = ({ row, seat, seatId, isBooked, isSelected, toggleSeat }) => {
   const handleSeatClick = () => {
-    toggleSeat(row, seat);
+    toggleSeat(row, seat, seatId);
   };
 
-  const SeatIcon = isSelected ? ChairIcon : ChairOutlinedIcon;
+  const SeatIcon = isBooked || isSelected ? ChairIcon : ChairOutlinedIcon;
 
   return <SeatIcon onClick={handleSeatClick} fontSize="large" />;
 };
