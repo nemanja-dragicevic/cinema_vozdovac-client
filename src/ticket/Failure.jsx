5ef062fdee990822c430ace227565b340b63925d
nemanja-dragicevic <nd201900065@student.fon.bg.ac.rs>
2024-07-07 16:36:38 +0200

    changed look of pages after saving ticket

--- src/ticket/Failure.jsx
+++ src/ticket/Failure.jsx
@@ -1,7 +1,30 @@
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { error } from "../utils/notification";
+import "../styles/gradient.css";
+
 const Failure = () => {
+  useEffect(() => {
+    error("Payment cancelled! Redirecting to home page...");
+    setTimeout(() => {
+      window.location.href = "/";
+    }, 3000);
+  }, []);
+
   return (
-    <div>
-      <h1>Failure</h1>
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1 className="gradient-text">Payment Cancelled</h1>
+      <p>
+        Your ticket payment was cancelled. You will be redirected to the home
+        page shortly.
+      </p>
+      <p>
+        If you are not redirected, click{" "}
+        <Link to="/" style={{ color: "blue" }}>
+          here
+        </Link>{" "}
+        to go to the home page.
+      </p>
     </div>
   );
 };
--- src/ticket/Sucess.jsx
+++ src/ticket/Sucess.jsx
@@ -1,6 +1,8 @@
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import * as ticketActions from "../actions/tickets";
+import "../styles/gradient.css";
+import { Link } from "react-router-dom";
 
 const Success = () => {
   const dispatch = useDispatch();
@@ -14,8 +16,19 @@ const Success = () => {
   }, [sessionID]);
 
   return (
-    <div>
-      <h1>Success</h1>
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1 className="gradient-text">Payment Successfull</h1>
+      <p>
+        Your ticket payment will be processed. You will be redirected to the
+        home page shortly.
+      </p>
+      <p>
+        If you are not redirected, click{" "}
+        <Link to="/" style={{ color: "blue" }}>
+          here
+        </Link>{" "}
+        to go to the home page.
+      </p>
     </div>
   );
 };
